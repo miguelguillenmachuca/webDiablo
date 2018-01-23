9449b267232b7f22e5ab8a95d3652dd5fcf7eda6
Miguel Guillén Machuca <miguelguillensmr@gmail.com>
2018-01-23 02:56:52 +0100

    ** Añadido de la migración Runa **

---
+++ database/migrations/2018_01_23_015116_create_runa_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRunaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('runa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_habilidad')->unsigned();
+            $table->string('nombre', 50);
+            $table->string('descripcion', 200);
+            $table->string('foto_runa', 254)->default('runas/default_rune.png');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_habilidad')->references('id')->on('habilidad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign('runa_id_habilidad_foreign');
+
+        Schema::dropIfExists('runa');
+    }
+}
