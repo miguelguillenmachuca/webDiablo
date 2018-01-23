622b819c71ee0f49f553b41947dd87098d1fa884
Miguel Guillén Machuca <miguelguillensmr@gmail.com>
2018-01-23 03:08:14 +0100

    ** Añadido de la migración Guia **

---
+++ database/migrations/2018_01_23_015735_create_guia_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuiaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guia', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 70);
+            $table->integer('id_usuario')->unsigned();
+            $table->integer('id_clase')->unsigned();
+            $table->enum('estado', ['publica', 'privada']);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_usuario')->references('id')->on('users');
+            $table->foreign('id_clase')->references('id')->on('clase');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign('guia_id_clase_foreign');
+        $table->dropForeign('guia_id_usuario_foreign');
+
+        Schema::dropIfExists('guia');
+    }
+}
