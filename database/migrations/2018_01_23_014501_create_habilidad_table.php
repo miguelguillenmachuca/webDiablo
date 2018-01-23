3ae89ade73c2ccdf1badb30792943cbca4040e0e
Miguel Guillén Machuca <miguelguillensmr@gmail.com>
2018-01-23 02:50:47 +0100

    ** Añadido de la migración Habilidad **

---
+++ database/migrations/2018_01_23_014501_create_habilidad_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHabilidadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('habilidad', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 50);
+            $table->enum('tipo_habilidad', ['activa', 'pasiva']);
+            $table->integer('id_clase')->unsigned();
+            $table->string('descripcion', 200);
+            $table->string('foto_habilidad', 254)->default('habilidades/default_skill.png');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_clase')->references('id')->on('clase');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign('habilidad_id_clase_foreign');
+        
+        Schema::dropIfExists('habilidad');
+    }
+}
