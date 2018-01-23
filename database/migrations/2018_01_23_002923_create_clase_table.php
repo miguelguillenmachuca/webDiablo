cc2ee811dd20c001d8f507bbebd8a6d1e4bd1d42
Miguel Guillén Machuca <miguelguillensmr@gmail.com>
2018-01-23 01:45:17 +0100

    ** Migración de la tabla Clase **

---
+++ database/migrations/2018_01_23_002923_create_clase_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClaseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clase', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 100);
+            $table->string('foto_clase', 254)->default('clases/default_class.png');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clase');
+    }
+}
