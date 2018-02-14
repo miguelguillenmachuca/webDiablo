057203db222ad2643cb93cc5ff634ef9d5d08178
Miguel Guillén Machuca <miguelguillensmr@gmail.com>
2018-02-14 19:08:28 +0100

    ** Añadido del panel de usuario en el navbar **

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,14 +60,18 @@
                             <li class="{{ isActiveUrl('register') }}"><a href="{{ route('register') }}">Registro</a></li>
                         @else
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                <a href="#" class="dropdown-toggle profile-image" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    <img src="{{ asset('img/' .Auth::user()->foto_usuario) }}" alt="foto_usuario" class="img-circle img-usuario-navbar">
+                                    {{ Auth::user()->nombre }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li><a href="">Panel de cuenta</a></li>
                                     <li><a href="">Mis guías</a></li>
                                     <li><a href="">Mis guías favoritas</a></li>
+                                    @if(Auth::user()->tipo_usuario == 'administrador')
+                                      <li><a href="{{ route('admin') }}">Panel de administración</a></li>
+                                    @endif
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
