0c010ab196dd75353e169733c16cee0e53c533ba
Miguel Guillén Machuca <miguelguillensmr@gmail.com>
2018-04-02 22:00:58 +0200

    ** Completada la vista admin_objetos_conjuntos_efectos **

--- resources/views/admin_objetos_conjuntos_efectos.blade.php
+++ resources/views/admin_objetos_conjuntos_efectos.blade.php
@@ -17,23 +17,7 @@
 
           <div class="col-xs-12 col-sm-6 col-sm-pull-6 margen-sup">
             <div>
-              <ul class="pagination pagination-redondo">
-                <li>
-                  <a href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li class="active"><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-                <li>
-                  <a href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
+              {{ $efectos_conjuntos->links('pagination.limit_links') }}
             </div> <!-- Fin del div paginado -->
           </div>
         </div> <!-- Fin del div row -->
@@ -55,32 +39,16 @@
             </thead>
 
             <tbody>
-              <tr class="inactive">
-                <td>conjunto1</td>
-                <td>2</td>
-                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat porro, nobis totam et, voluptatibus delectus praesentium iure iusto consequatur tempore!</td>
-                <td><a href="{{ route('admin/objetos/conjuntos/efectos/editar', ['1']) }}"><span class="glyphicon glyphicon-pencil boton-edit"></span></a></td>
-                <td class="inactive"><a href="#"><span class="glyphicon glyphicon-remove boton-remove"></span></a></td>
-                <td><a href="#"><span class="glyphicon glyphicon-repeat boton-restore"></span></a></td>
-              </tr>
-
-              <tr>
-                <td>conjunto1</td>
-                <td>2</td>
-                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat porro, nobis totam et, voluptatibus delectus praesentium iure iusto consequatur tempore!</td>
-                <td><a href="{{ route('admin/objetos/conjuntos/efectos/editar', ['1']) }}"><span class="glyphicon glyphicon-pencil boton-edit"></span></a></td>
-                <td><a href="#"><span class="glyphicon glyphicon-remove boton-remove"></span></a></td>
-                <td class="inactive"><a href="#"><span class="glyphicon glyphicon-repeat boton-restore"></span></a></td>
-              </tr>
-
-              <tr>
-                <td>conjunto1</td>
-                <td>2</td>
-                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat porro, nobis totam et, voluptatibus delectus praesentium iure iusto consequatur tempore!</td>
-                <td><a href="{{ route('admin/objetos/conjuntos/efectos/editar', ['1']) }}"><span class="glyphicon glyphicon-pencil boton-edit"></span></a></td>
-                <td><a href="#"><span class="glyphicon glyphicon-remove boton-remove"></span></a></td>
-                <td class="inactive"><a href="#"><span class="glyphicon glyphicon-repeat boton-restore"></span></a></td>
-              </tr>
+              @foreach ($efectos_conjuntos as $efecto_conjunto)
+                <tr class="{{ $efecto_conjunto->trashed() ? 'inactive' : '' }}">
+                  <td>{{ $efecto_conjunto->conjunto->nombre }}</td>
+                  <td>{{ $efecto_conjunto->num_requisito }}</td>
+                  <td>{{ $efecto_conjunto->efecto }}</td>
+                  <td><a href="{{ route('admin/objetos/conjuntos/efectos/editar', [ $efecto_conjunto ]) }}"><span class="glyphicon glyphicon-pencil boton-edit"></span></a></td>
+                  <td class="{{ $efecto_conjunto->trashed() ? 'inactive' : '' }}"><a href="{{ $efecto_conjunto->trashed() ? '#' : route('admin/deleteEfectoConjunto', [ $efecto_conjunto ]) }}"><span class="glyphicon glyphicon-remove boton-remove"></span></a></td>
+                  <td class="{{ $efecto_conjunto->trashed() ? '' : 'inactive' }}"><a href="{{ !$efecto_conjunto->trashed() ? '#' : route('admin/restoreEfectoConjunto', [ $efecto_conjunto->getRouteKey() ]) }}"><span class="glyphicon glyphicon-repeat boton-restore"></span></a></td>
+                </tr>
+              @endforeach
             </tbody>
           </table>
         </div>
@@ -88,23 +56,7 @@
 
       <div class="col-xs-12">
         <div>
-          <ul class="pagination pagination-redondo">
-            <li>
-              <a href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-              </a>
-            </li>
-            <li class="active"><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-            <li>
-              <a href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-              </a>
-            </li>
-          </ul>
+          {{ $efectos_conjuntos->links('pagination.limit_links') }}
         </div> <!-- Fin del div paginado -->
       </div>
     </div> <!-- Fin del div row -->
