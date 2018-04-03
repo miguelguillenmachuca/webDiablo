c10c1e610f90db5c10d474724004233c10b9a93d
Miguel Guillén Machuca <miguelguillensmr@gmail.com>
2018-04-03 16:47:57 +0200

    ** Creación del modelo Guia y del controlador GuiaController **

---
+++ app/Http/Controllers/GuiasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Guia;
+use Illuminate\Http\Request;
+
+class GuiasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Guia  $guia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Guia $guia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Guia  $guia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Guia $guia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Guia  $guia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Guia $guia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Guia  $guia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Guia $guia)
+    {
+        //
+    }
+}
