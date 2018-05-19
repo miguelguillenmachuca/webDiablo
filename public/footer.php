f2987da48dd333268050b907f3dd0a9bcde4ce89
Miguel Guillén Machuca <miguelguillensmr@gmail.com>
2018-05-19 23:36:15 +0200

    ** Limpieza de carpeta public **

--- public/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer footer-inverse">
-  <div class="container-fluid">
-    <div class="row">
-      <span id="derechos" class="col-xs-12 col-md-8">&copy; Miguel Guillén Machuca 2016</span>
-      <span id="contacto" class="col-xs-12 col-md-4 text-right">miguelguillensmr@gmail.com</span>
-    </div>
-  </div>
-</footer>
